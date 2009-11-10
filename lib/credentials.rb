c1989a370868aa8261eafee0aecd34b28c5ffbcb
Matt Powell <fauxparse@gmail.com>
2009-11-11 09:17:47 +1300

    clean slate for v2

--- lib/credentials.rb
+++
@@ -1,9 +0,0 @@
-require "credentials/actor"
-require "credentials/rulebook"
-require "credentials/class_methods"
-require "credentials/inflector"
-require "credentials/rules/rule"
-Dir.glob(File.dirname(__FILE__) + "/credentials/rules/*.rb").each { |f| require f }
-Dir.glob(File.dirname(__FILE__) + "/credentials/support/*.rb").each { |f| require f } unless defined?(ActiveSupport)
-
-Object.send :extend, Credentials::ClassMethods
