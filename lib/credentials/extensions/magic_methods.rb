125896e0fdb4d86a06160380b93f0dbc655cacf7
Matt Powell <fauxparse@gmail.com>
2009-11-25 13:43:18 +1300

    disable magic methods

--- lib/credentials/extensions/magic_methods.rb
+++ lib/credentials/extensions/magic_methods.rb
@@ -12,17 +12,17 @@ module Credentials
     #     user = User.new
     #     user.can_edit? user #=> true
     module MagicMethods
-      def self.included(receiver) #:nodoc:
-        receiver.class_eval do
-          def method_missing(sym, *args)
-            if self.class != Object && self.class != Class && sym.to_s =~ /\Acan_(.*)\?\z/
-              can? $1.to_sym, *args
-            else
-              super
-            end
-          end
-        end
-      end
+      # def self.included(receiver) #:nodoc:
+      #   receiver.class_eval do
+      #     def method_missing(sym, *args)
+      #       if self.class != Object && self.class != Class && sym.to_s =~ /\Acan_(.*)\?\z/
+      #         can? $1.to_sym, *args
+      #       else
+      #         super
+      #       end
+      #     end
+      #   end
+      # end
     end
   end
 end
