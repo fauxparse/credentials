4267b34fe4134c8f441bd2a814f9144673cc2c7f
Matt Powell <fauxparse@gmail.com>
2010-01-26 11:31:43 +1300

    fix to get constantize sans ActiveSupport

--- lib/credentials.rb
+++ lib/credentials.rb
@@ -11,3 +11,31 @@ Object.send :include, Credentials::Extensions::Object
 if defined?(ActionController)
   ActionController::Base.send :include, Credentials::Extensions::ActionController
 end
+
+unless defined?(ActiveSupport)
+  class String
+    if Module.method(:const_get).arity == 1
+      def constantize
+        names = split('::')
+        names.shift if names.empty? || names.first.empty?
+
+        constant = Object
+        names.each do |name|
+          constant = constant.const_defined?(name) ? constant.const_get(name) : constant.const_missing(name)
+        end
+        constant
+      end
+    else
+      def constantize
+        names = split('::')
+        names.shift if names.empty? || names.first.empty?
+
+        constant = Object
+        names.each do |name|
+          constant = constant.const_get(name, false) || constant.const_missing(name)
+        end
+        constant
+      end
+    end
+  end
+end
